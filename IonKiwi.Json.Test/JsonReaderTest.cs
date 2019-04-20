1072ea1b29acb8e8c69fe40b4a0b0f57e83889f0
Ewout van der Linden <IonKiwi@users.noreply.github.com>
2019-04-20 15:30:08 +0200

    complete test

--- IonKiwi.Json.Test/JsonReaderTest.cs
+++ IonKiwi.Json.Test/JsonReaderTest.cs
@@ -15,6 +15,27 @@ namespace IonKiwi.Json.Test {
 			Assert.Equal(1, reader.CharacterPosition);
 			Assert.Equal(1, reader.LineNumber);
 
+			var token = reader.ReadSync();
+			Assert.Equal(JsonToken.ObjectStart, token);
+			Assert.Equal(1, reader.Depth);
+			Assert.Equal(string.Empty, reader.GetPath());
+			Assert.Equal(2, reader.CharacterPosition);
+			Assert.Equal(1, reader.LineNumber);
+
+			token = reader.ReadSync();
+			Assert.Equal(JsonToken.ObjectEnd, token);
+			Assert.Equal(0, reader.Depth);
+			Assert.Equal(string.Empty, reader.GetPath());
+			Assert.Equal(3, reader.CharacterPosition);
+			Assert.Equal(1, reader.LineNumber);
+
+			token = reader.ReadSync();
+			Assert.Equal(JsonToken.None, token);
+			Assert.Equal(0, reader.Depth);
+			Assert.Equal(string.Empty, reader.GetPath());
+			Assert.Equal(3, reader.CharacterPosition);
+			Assert.Equal(1, reader.LineNumber);
+
 			return;
 		}
 	}
