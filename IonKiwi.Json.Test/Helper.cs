d42b0000f0acf6f9287b969f0ff01d147b3e344e
Ewout van der Linden <IonKiwi@users.noreply.github.com>
2019-04-27 11:21:31 +0200

    add test to create test code

---
+++ IonKiwi.Json.Test/Helper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace IonKiwi.Json.Test {
+	internal static class Helper {
+		internal static byte[] GetStringData(string resourceName) {
+			var asm = typeof(Helper).Assembly;
+			var asmName = asm.GetName(false);
+			var fullResourceName = asmName.Name + ".Resources." + resourceName;
+			using (var s = asm.GetManifestResourceStream(fullResourceName)) {
+				if (s == null) {
+					throw new InvalidOperationException($"Resource '{resourceName}' not found.");
+				}
+				using (var ms = new MemoryStream()) {
+					s.CopyTo(ms);
+					return ms.ToArray();
+				}
+			}
+		}
+	}
+}
--- IonKiwi.Json.Test/JsonReaderTest.cs
+++ IonKiwi.Json.Test/JsonReaderTest.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Globalization;
+using System.IO;
 using System.Text;
 using Xunit;
 using static IonKiwi.Json.JsonReader;
@@ -36,6 +38,40 @@ namespace IonKiwi.Json.Test {
 			Assert.Equal(3, reader.CharacterPosition);
 			Assert.Equal(1, reader.LineNumber);
 
+			return;
+		}
+
+		[Fact]
+		public void CreateTest() {
+			byte[] json = Helper.GetStringData("Object1.json");
+
+			var reader = new JsonReader(new Utf8ByteArrayInputReader(json));
+
+			StringBuilder sb = new StringBuilder();
+			sb.AppendLine("JsonToken token;");
+			sb.AppendLine("var reader = new JsonReader(new Utf8ByteArrayInputReader(Encoding.UTF8.GetBytes(json)));");
+			sb.AppendLine("Assert.Equal(0, reader.Depth);");
+			sb.AppendLine("Assert.Equal(string.Empty, reader.GetPath());");
+			sb.AppendLine("Assert.Equal(1, reader.CharacterPosition);");
+			sb.AppendLine("Assert.Equal(1, reader.LineNumber);");
+
+			do {
+				var token = reader.ReadSync();
+				sb.AppendLine();
+				sb.AppendLine("token = reader.ReadSync();");
+				sb.AppendLine("Assert.Equal(JsonToken." + token.ToString() + ", token);");
+				sb.AppendLine("Assert.Equal(" + reader.Depth.ToString(CultureInfo.InvariantCulture) + ", reader.Depth);");
+				sb.AppendLine("Assert.Equal(\"" + reader.GetPath().Replace("\"", "\\\"") + "\", reader.GetPath());");
+				sb.AppendLine("Assert.Equal(" + reader.CharacterPosition.ToString(CultureInfo.InvariantCulture) + ", reader.CharacterPosition);");
+				sb.AppendLine("Assert.Equal(" + reader.LineNumber.ToString(CultureInfo.InvariantCulture) + ", reader.LineNumber);");
+
+				if (token == JsonToken.None) {
+					break;
+				}
+			}
+			while (true);
+
+			string testCode = sb.ToString();
 			return;
 		}
 	}
