4f162c37bdec77a0ca5ff4ceba7c8bc22d2c7cf5
Ewout van der Linden <IonKiwi@users.noreply.github.com>
2019-05-12 13:44:48 +0200

    add initial JsonParser class

---
+++ IonKiwi.Json/JsonParser.cs
@@ -0,0 +1,50 @@
+﻿using IonKiwi.Extenions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using static IonKiwi.Json.JsonReader;
+
+namespace IonKiwi.Json {
+	public static partial class JsonParser {
+
+		public static ValueTask<T> Parse<T>(JsonReader reader) {
+			return Parse<T>(reader, typeof(T));
+		}
+
+		public static T ParseSync<T>(JsonReader reader) {
+			return ParseSync<T>(reader, typeof(T));
+		}
+
+		public static async ValueTask<T> Parse<T>(JsonReader reader, Type objectType) {
+
+			JsonParserRootState state = new JsonParserRootState();
+
+			JsonToken token;
+			do {
+				token = await reader.Read().NoSync();
+				HandleToken(token);
+			}
+			while (token != JsonToken.None);
+
+			return (T)state.Value;
+		}
+
+		public static T ParseSync<T>(JsonReader reader, Type objectType) {
+			JsonParserRootState state = new JsonParserRootState();
+
+			JsonToken token;
+			do {
+				token = reader.ReadSync();
+				HandleToken(token);
+			}
+			while (token != JsonToken.None);
+
+			return (T)state.Value;
+		}
+
+		private static void HandleToken(JsonToken token) {
+
+		}
+	}
+}
---
+++ IonKiwi.Json/JsonParserInternalState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IonKiwi.Json {
+	partial class JsonParser {
+
+		private class JsonParserInternalState {
+
+		}
+
+		private sealed class JsonParserRootState : JsonParserInternalState {
+			public object Value;
+		}
+	}
+}
