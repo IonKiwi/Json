c1d507ef6c6e860a72dffeae542a4d4cc8df2861
Ewout van der Linden <IonKiwi@users.noreply.github.com>
2019-05-12 13:57:36 +0200

    add initial reflection class

---
+++ IonKiwi.Json/JsonReflection.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace IonKiwi.Json {
+	internal static class JsonReflection {
+
+		private static readonly object _syncRoot = new object();
+		private static Dictionary<Type, JsonTypeInfo> _typeInfo = new Dictionary<Type, JsonTypeInfo>();
+
+		internal class JsonTypeInfo {
+			public Type ValueType { get; set; }
+		}
+
+		public static JsonTypeInfo GetTypeInfo(Type t) {
+			if (_typeInfo.TryGetValue(t, out var typeInfo)) {
+				return typeInfo;
+			}
+
+			lock (_syncRoot) {
+				if (_typeInfo.TryGetValue(t, out typeInfo)) {
+					return typeInfo;
+				}
+
+				typeInfo = CreateTypeInfo(t);
+
+				var newTypeInfo = new Dictionary<Type, JsonTypeInfo>();
+				foreach (var kv in _typeInfo) {
+					newTypeInfo.Add(kv.Key, kv.Value);
+				}
+				newTypeInfo.Add(t, typeInfo);
+
+				Interlocked.MemoryBarrier();
+				_typeInfo = newTypeInfo;
+			}
+
+			return typeInfo;
+		}
+
+		private static JsonTypeInfo CreateTypeInfo(Type t) {
+			var ti = new JsonTypeInfo();
+			ti.ValueType = t;
+
+			return ti;
+		}
+	}
+}
---
+++ IonKiwi.Json/MetaData/JsonObjectAttribute.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IonKiwi.Json.MetaData {
+
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+	public class JsonObjectAttribute : Attribute {
+
+	}
+
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+	public class JsonCollectionAttribute : Attribute {
+
+	}
+
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+	public class JsonDictionaryAttribute : Attribute {
+
+	}
+
+	public enum JsonEmitTypeName {
+		DifferentType,
+		None,
+		Always,
+	}
+
+	[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+	public class JsonPropertyAttribute : Attribute {
+
+		public JsonPropertyAttribute() { }
+
+		public JsonPropertyAttribute(string name = null, JsonEmitTypeName emitTypeName = JsonEmitTypeName.DifferentType) {
+			Name = name;
+			EmitTypeName = emitTypeName;
+		}
+
+		public string Name { get; set; }
+
+		public JsonEmitTypeName EmitTypeName { get; set; }
+	}
+}
