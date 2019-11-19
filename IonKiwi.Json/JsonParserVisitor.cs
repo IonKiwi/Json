b29bd4f4a7964b0cad6186f950585eccc6feb131
Ewout van der Linden <IonKiwi@users.noreply.github.com>
2019-11-19 21:21:44 +0100

    add option to specify multiple visitors

--- IonKiwi.Json/JsonParserVisitor.cs
+++ IonKiwi.Json/JsonParserVisitor.cs
@@ -63,6 +63,10 @@ namespace IonKiwi.Json {
 		protected abstract bool ParseObject(IJsonReader reader, JsonParserContext context);
 
 		void IJsonParserVisitor.Initialize(JsonParserSettings parserSettings) {
+			InitializeInternal(parserSettings);
+		}
+
+		internal virtual void InitializeInternal(JsonParserSettings parserSettings) {
 			_parserSettings = parserSettings.Clone();
 			_parserSettings.Visitor = null;
 		}
@@ -77,6 +81,104 @@ namespace IonKiwi.Json {
 		Task<bool> IJsonParserVisitor.ParseObjectAsync(IJsonReader reader, JsonParserContext context) {
 #endif
 			return ParseObjectAsync(reader, context);
+		}
+	}
+
+	public sealed class JsonParserVisitors : JsonParserVisitor {
+
+		private readonly List<IJsonParserVisitor> _visitors = new List<IJsonParserVisitor>();
+
+		public JsonParserVisitors() {
+
+		}
+
+		public JsonParserVisitors(params JsonParserVisitor[] visitors) {
+			_visitors.AddRange(visitors);
+		}
+
+		public void Add(JsonParserVisitor visitor) {
+			_visitors.Add(visitor);
+		}
+
+		public void AddRange(IEnumerable<JsonParserVisitor> visitors) {
+			_visitors.AddRange(visitors);
+		}
+
+		internal override void InitializeInternal(JsonParserSettings parserSettings) {
+			base.InitializeInternal(parserSettings);
+			foreach (var visitor in _visitors) {
+				visitor.Initialize(parserSettings);
+			}
+		}
+
+		protected override bool ParseObject(IJsonReader reader, JsonParserContext context) {
+			foreach (var visitor in _visitors) {
+				if (visitor.ParseObject(reader, context)) {
+					return true;
+				}
+			}
+			return false;
+		}
+
+#if !NET472
+		protected override ValueTask<bool> ParseObjectAsync(IJsonReader reader, JsonParserContext context) {
+#else
+		protected override Task<bool> ParseObjectAsync(IJsonReader reader, JsonParserContext context) {
+#endif
+			IEnumerator<IJsonParserVisitor> e = _visitors.GetEnumerator();
+			IDisposable d = e;
+			try {
+				while (e.MoveNext()) {
+					var task = e.Current.ParseObjectAsync(reader, context);
+#if !NET472
+					if (!task.IsCompletedSuccessfully) {
+#else
+					if (task.Status != TaskStatus.RanToCompletion) {
+#endif
+						d = null;
+						return ParseObjectAsyncContinue(task, e, reader, context);
+					}
+					else if (task.Result) {
+#if !NET472
+						return new ValueTask<bool>(true);
+#else
+						return Task.FromResult(true);
+#endif
+					}
+				}
+			}
+			finally {
+				if (d != null) {
+					d.Dispose();
+				}
+			}
+#if !NET472
+			return new ValueTask<bool>(false);
+#else
+			return Task.FromResult(false);
+#endif
+		}
+
+#if !NET472
+		private async ValueTask<bool> ParseObjectAsyncContinue(ValueTask<bool> task, IEnumerator<IJsonParserVisitor> e, IJsonReader reader, JsonParserContext context) {
+#else
+		private async Task<bool> ParseObjectAsyncContinue(Task<bool> task, IEnumerator<IJsonParserVisitor> e, IJsonReader reader, JsonParserContext context) {
+#endif
+			IDisposable d = e;
+			try {
+				if (await task.ConfigureAwait(false)) {
+					return true;
+				}
+				while (e.MoveNext()) {
+					if (await e.Current.ParseObjectAsync(reader, context).ConfigureAwait(false)) {
+						return true;
+					}
+				}
+			}
+			finally {
+				d.Dispose();
+			}
+			return false;
 		}
 	}
 }
